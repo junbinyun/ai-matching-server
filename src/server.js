b25e05250cadcd4a6e455983dc2d76ac5a29228c
junbinyun <dufrt96@gmail.com>
2026-05-20 23:06:54 +0900

    server.js
    .

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 
 // -----------------------------
 // 기본 미들웨어 설정
